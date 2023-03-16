970e00b778f5b67262ffc007713497ae0bee99ee
Kritan Thapa <kritant37@gmail.com>
2023-03-16 07:26:39 +0545

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	let mtr = 0;
+	$: inch = 2 * mtr;
+</script>
+
+<h1>Nepali Unit Converter</h1>
+<p>Enter data</p>
+
+<label for="meter">Meter</label>
+<input id="meter" type="text" bind:value={mtr} />
+
+<label for="inch">Inch</label>
+<input id="inch" type="text" bind:value={inch} />
