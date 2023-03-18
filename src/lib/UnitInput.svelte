90784ff150ac6b8ba81b751ed6353dd4ce96cbd4
Kritan Thapa <kritant37@gmail.com>
2023-03-18 15:41:05 +0545

    added custom label and styling

--- src/lib/UnitInput.svelte
+++ src/lib/UnitInput.svelte
@@ -1,8 +1,29 @@
 <script>
 	export let id;
 	export let value;
+	export let label = id;
+	export let unit = '';
 	export let onChangeHandler;
 </script>
 
-<label for="sqrft"> {id} : </label>
-<input type="text" {id} {value} on:input={onChangeHandler} />
+<div>
+	<label for="sqrft"> {label} : </label>
+	<input type="text" {id} {value} on:input={onChangeHandler} />
+	{@html unit}
+</div>
+
+<style>
+	input[type='text'] {
+		width: 80px;
+		padding: 6px;
+	}
+
+	label {
+		text-transform: capitalize;
+	}
+
+	div {
+		display: inline;
+		margin-right: 18px;
+	}
+</style>
