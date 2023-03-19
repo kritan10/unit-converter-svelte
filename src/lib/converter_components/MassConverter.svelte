7077d8a7ccc915db6e2c84d5784bb00e3f34655c
Kritan Thapa <kritant37@gmail.com>
2023-03-19 10:48:57 +0545

    added functionality

--- src/lib/converter_components/MassConverter.svelte
+++ src/lib/converter_components/MassConverter.svelte
@@ -3,9 +3,8 @@
 	import MathFunctions from '../MathFunctions.js';
 
 	let unit = {
-		lal: 0,
 		tola: 0,
-		chatak: 0,
+		gm: 0,
 		pau: 0,
 		dharni: 0,
 		kg: 0,
@@ -15,34 +14,48 @@
 	function onChangeHandler(event) {
 		unit[event.target.id] = +event.target.value;
 
-		if (event.target.id === 'lal' || event.target.id === 'tola' || event.target.id === 'chatak') {
+		if (event.target.id === 'tola') {
+			unit.gm = unit.tola * 11.6;
+			return;
+		}
+
+		if (event.target.id === 'gm') {
+			unit.tola = unit.gm / 11.6;
 			return;
 		}
 
 		if (event.target.id === 'pau' || event.target.id === 'dharni') {
+			unit.kg = unit.dharni * 2.39 + unit.pau * 0.2;
+			unit.lbs = unit.kg * 2.204;
 			return;
 		}
 
 		if (event.target.id === 'kg') {
+			let tempResult = MathFunctions.divide(unit.kg * 5, 12);
+			unit.pau = tempResult[0];
+			unit.dharni = tempResult[1];
 			return;
 		}
 		if (event.target.id === 'lbs') {
+			let tempResult = MathFunctions.divide(unit.lbs * 2.5, 12);
+			unit.pau = tempResult[0];
+			unit.dharni = tempResult[1];
 			return;
 		}
-
 	}
 </script>
 
 <div>
 	<h2>Weight Converter</h2>
 	<div>
-		<UnitInput id="lal" value={unit.lal} {onChangeHandler} />
+		<h3>Tola/Gram Converter</h3>
 		<UnitInput id="tola" value={unit.tola} {onChangeHandler} />
-		<UnitInput id="chatak" value={unit.chatak} {onChangeHandler} />
+		<UnitInput id="gm" value={unit.gm} {onChangeHandler} />
 	</div>
 	<div>
-		<UnitInput id="pau" value={unit.pau} {onChangeHandler} />
+		<h3>Pau/Dharni - Kg/Lbs Converter</h3>
 		<UnitInput id="dharni" value={unit.dharni} {onChangeHandler} />
+		<UnitInput id="pau" value={unit.pau} {onChangeHandler} />
 	</div>
 	<div>
 		<UnitInput id="kg" value={unit.kg} {onChangeHandler} />
