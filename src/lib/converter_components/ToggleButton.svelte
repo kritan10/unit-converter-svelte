0e009949cad6de3304858c50ed3d8498ee2bcdfb
Kritan Thapa <kritant37@gmail.com>
2023-03-19 10:49:46 +0545

    initial commit

---
+++ src/lib/converter_components/ToggleButton.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let button;
+	export let currentButton;
+	export let toggleButton;
+	export let buttonLabel;
+</script>
+
+<button class:active={button === currentButton} on:click={() => toggleButton(button)}>
+	{buttonLabel}
+</button>
+
+<style>
+	.active {
+		background-color: #ff3e00;
+		color: white;
+	}
+
+	button {
+		width: 100%;
+		padding: 20px 0px;
+		cursor: pointer;
+		border-radius: 0;
+	}
+</style>
