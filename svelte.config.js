a9610796105538f1f19d729c5e004741ac1724d8
Kritan Thapa <kritant37@gmail.com>
2023-03-21 13:33:56 +0545

    deployment changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
+		adapter: adapter()
+		// ... truncated ...
 	}
 };
+
+export default config;
